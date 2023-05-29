56f4f2c848fdd741e93aed911e5d8d18966690f3
Dhruv Sood <dhruvsoodboss@gmail.com>
2023-05-29 10:07:24 +0530

    Add ppt,report,readme

---
+++ readme.md
@@ -0,0 +1,35 @@
+
+# Survey Form
+
+This is a simple survey form project created using HTML, CSS, and JavaScript. The form allows users to answer a set of questions and submit their responses.
+
+
+
+
+## Table of content
+Features
+
+Demo
+
+Installation
+
+
+## Features
+
+Multiple-choice questions
+
+Text input fields
+
+Dropdown selection
+
+Basic styling with CSS
+## Installation
+
+Clone the repository:
+
+git clone https://github.com/Dhruv-Sood/Survey-form-project.git
+
+
+
+
+    
--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-let submitBtn = document.querySelector("#submitbtn")
-let name = document.querySelector("#name").value
-let email = document.querySelector("#email").value
-let address = document.querySelector("#address").value
-let contact = document.querySelector("#contact").value
-submitBtn.addEventListener("click" , ()=>{
-    if(name == "" && email == "" && address == "" && contact == ""){
-        alert("Form is empty")
-    }
-    else if (name == "") {
-      alert("Enter name in the form");
-    } else if (email == "") {
-      alert("Enter email in the form");
-    } else if (address == "") {
-      alert("Enter address in the form");
-    } else if (contact == "") {
-      alert("Enter contact in the form");
-    } else {
-      alert("Form filled succesfully!!");
-    }
-})
+let submitBtn = document.querySelector("#submitbtn");
+
+submitBtn.addEventListener("click", () => {
+    let name = document.querySelector("#name").value;
+    let email = document.querySelector("#email").value;
+    let address = document.querySelector("#address").value;
+    let contact = document.querySelector("#contact").value;
+  if (name == "" && email == "" && address == "" && contact == "") {
+    alert("Form is empty");
+  } else if (name == "") {
+    alert("Enter name in the form");
+  } else if (email == "") {
+    alert("Enter email in the form");
+  } else if (address == "") {
+    alert("Enter address in the form");
+  } else if (contact == "") {
+    alert("Enter contact in the form");
+  } else {
+    alert("Form filled succesfully!!");
+  }
+});
